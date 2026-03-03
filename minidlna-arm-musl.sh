f1e236b804d79c2ed323e0f02eba80533368d0fe
Richard Elwell <229418400@users.noreply.github.com>
2026-03-03 14:59:17 -0500

    updated shared code

--- minidlna-arm-musl.sh
+++ minidlna-arm-musl.sh
@@ -85,6 +85,9 @@ STAGE_DIR="${CROSSBUILD_DIR}/stage/${PKG_ROOT}"
 PACKAGER_NAME="${PKG_ROOT}_${PKG_ROOT_VERSION}-${PKG_ROOT_RELEASE}_${PKG_TARGET_CPU}${PKG_TARGET_VARIANT}"
 PACKAGER_ROOT="${CROSSBUILD_DIR}/packager/${PKG_ROOT}/${PACKAGER_NAME}"
 PACKAGER_TOPDIR="${PACKAGER_ROOT}/${PKG_ROOT}-${PKG_ROOT_VERSION}"
+PORTABLE_DIR="/tmp/portable-${PKG_ROOT}"
+SYSROOT_PORTABLE_DIR="${SYSROOT}/tmp/portable-${PKG_ROOT}"
+PREFIX_PORTABLE_DIR="${SYSROOT_PORTABLE_DIR}"
 
 MAKE="make -j$(grep -c ^processor /proc/cpuinfo)" # parallelism
 #MAKE="make -j1"                                  # one job at a time
@@ -94,10 +97,6 @@ export PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig"
 unset PKG_CONFIG_PATH
 
 install_build_environment
-
-if ${MINIDLNA_THUMBNAILS_ENABLED}; then
-    create_cmake_toolchain_file
-fi
 
 download_and_compile
 
@@ -183,8 +182,8 @@ handle_configure_error()
 
     #grep -R --include="config.log" --color=always "undefined reference" .
     #find . -name "config.log" -exec grep -H "undefined reference" {} \;
-    find . -name "config.log" -exec grep -H -E "undefined reference|can't load library|unrecognized command-line option|No such file or directory" {} \;
-    #find . -name "config.log" -exec grep -H -E "undefined reference|can't load library|unrecognized command-line option" {} \;
+    #find . -name "config.log" -exec grep -H -E "undefined reference|can't load library|unrecognized command-line option|No such file or directory" {} \;
+    find . -name "config.log" -exec grep -H -E "undefined reference|can't load library|unrecognized command-line option" {} \;
 
     # Force failure if rc is zero, since error was detected
     [ "${rc}" -eq 0 ] && return 1
@@ -1119,6 +1118,7 @@ download_and_compile() {
 export PATH="${CROSSBUILD_DIR}/bin:${PATH}"
 mkdir -p "${SRC_ROOT}"
 #mkdir -p "${STAGE_DIR}"
+create_cmake_toolchain_file
 
 ################################################################################
 # zlib-1.3.1
